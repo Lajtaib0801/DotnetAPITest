d31584ca272561f0a4df0b39b72970b21ba438aa
Lajtai Benjámin <lajtaibenja@gmail.com>
2024-06-06 22:50:20 +0200

    Update

---
+++ Controllers/StockController.cs
@@ -0,0 +1,35 @@
+﻿using DotnetAPITest.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetAPITest.Controllers
+{
+    [Route("api/stock")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public StockController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var stocks = _context.Stock.ToList();
+            return Ok(stocks);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            var stock = _context.Stock.Find(id);
+
+            if (stock is null)
+            {
+                return NotFound();
+            }
+            return Ok(stock);
+        }
+    }
+}
