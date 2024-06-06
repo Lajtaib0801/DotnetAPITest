67e48d64c9570ec2f546f6f55bbbce51ed544333
lajtaib <lajtaib@MAIN>
2024-06-06 22:18:38 +0200

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using DotnetAPITest.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotnetAPITest.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Stock> Stock { get; set; }
+        public DbSet<Comment> Comment { get; set; }
+        public ApplicationDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions) { }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+using DotnetAPITest.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    string? constr = builder.Configuration.GetConnectionString("DefaultConnection");
+    options.UseMySql(constr, ServerVersion.AutoDetect(constr));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
